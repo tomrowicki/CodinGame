77936e7004810f8967bd471e1997141467559e04
Tomasz Mrowicki <Tomasz Mrowicki@Bestia>
2016-05-03 11:48:53 +0200

    Initial working version.

--- SmashTheCode/src/smashthecode/Player.java
+++ SmashTheCode/src/smashthecode/Player.java
@@ -1,63 +1,92 @@
 package smashthecode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
-public class Player
+class Player
 {
-
     public static void main( String[] args )
     {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner( System.in );
 
         // game loop
-        while (true) {
+        while ( true )
+        {
             int[] nextEightColors = new int[8];
-            for (int i = 0; i < 8; i++) {
+            for ( int i = 0; i < 8; i++ )
+            {
                 int colorA = in.nextInt(); // color of the first block
                 nextEightColors[i] = colorA;
                 int colorB = in.nextInt(); // color of the attached block
             }
-            List<Character> myTopBlocks = new ArrayList<Character>();
-            for (int i = 0; i < 12; i++) {
+            List<Character> myTopBlocks = initializeEmptyBlockList();
+            for ( int i = 0; i < 12; i++ )
+            {
                 String row = in.next();
                 myTopBlocks = getTopBlocks( row, myTopBlocks );
             }
-            for (int i = 0; i < 12; i++) {
-                String row = in.next(); // One line of the map ('.' = empty, '0' = skull block, '1' to '5' = colored block)
+            for ( int i = 0; i < 12; i++ )
+            {
+                String row = in.next(); // One line of the map ('.' = empty, '0' = skull block, '1'
+                                        // to '5' = colored block)
             }
-            
+
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
 
-//            System.out.println("0"); // "x": the column in which to drop your blocks
-            System.out.println( decideAction(myTopBlocks, nextEightColors) );
-        }
-    }
-    
-    private static int decideAction( List<Character> myTopBlocks, int[] nextEightColors )
-    {
-        if (myTopBlocks.contains( new Character((char) nextEightColors[0]))) {
-            return myTopBlocks.indexOf( nextEightColors[0] );
-        } else {
-            return myTopBlocks.indexOf( '.' );
+            // System.out.println("0"); // "x": the column in which to drop your blocks
+            System.out.println( decideAction( myTopBlocks, nextEightColors ) );
         }
     }
 
-    private static List<Character> getTopBlocks(String row, List<Character> topBlocks) {
-        char[]blocks = row.toCharArray();
-//        List<Character> charList = new ArrayList<Character>();
-//        for(char block : blocks) {
-//            charList.add(block);
-//        }
-        
-        for (int i=0; i<blocks.length; i++) {
-            if (topBlocks.get( i ) == null && blocks[i] != '.') {
+    private static List<Character> initializeEmptyBlockList()
+    {
+        List<Character> emptyList = new ArrayList<Character>();
+        for ( int i = 0; i < 5; i++ )
+        {
+            emptyList.add( null );
+        }
+        return emptyList;
+    }
+
+    private static int decideAction( List<Character> myTopBlocks, int[] nextEightColors )
+    {
+        char nextColor = Integer.toString( nextEightColors[0] ).charAt( 0 );
+        char skull = Integer.toString( 0 ).charAt( 0 );
+        System.err.println( "Next color: " + nextColor );
+        if ( myTopBlocks.contains( nextColor ) )
+        {
+            System.err.println( "Returning block position." );
+            return myTopBlocks.indexOf(nextColor);
+        }
+        else
+        {
+            System.err.println( "Returning empty position." );
+            if (myTopBlocks.indexOf( null ) > -1) {
+                return myTopBlocks.indexOf( null );
+            } else {
+                return myTopBlocks.indexOf( skull);
+            }
+        }
+    }
+
+    private static List<Character> getTopBlocks( String row, List<Character> topBlocks )
+    {
+        System.err.println( "Current row: " + row );
+        char[] blocks = row.toCharArray();
+
+        for ( int i = 0; i < blocks.length; i++ )
+        {
+            System.err.println( "Block in check: " + blocks[i] );
+            if ( topBlocks.get( i ) == null && blocks[i] != '.' )
+            {
+                System.err.println( "Adding block to topList." );
+                topBlocks.remove( i );
                 topBlocks.add( i, blocks[i] );
             }
         }
+        System.err.println( "Top blocks: " + topBlocks );
         return topBlocks;
     }
 }
--- SmashTheCode/src/smashthecode/Tests.java
+++ SmashTheCode/src/smashthecode/Tests.java
@@ -1,5 +1,7 @@
 package smashthecode;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Tests
@@ -7,15 +9,41 @@ public class Tests
 
     public static void main( String[] args )
     {
-        String testString = "123456";
-        getTopBlocks(testString);
+//        String testString = "123456";
+//        getTopBlocks(testString);
+        
+//        List<Object> list = new ArrayList<Object>();
+//        for (int i=0; i < 6; i++) {
+//            list.add( null );
+//        }
+//        list.add( 3, "abc" );
+//        System.out.println( list.toString() );
+        
+        compareChars();
     }
     
-    static Map<String, Object> getTopBlocks(String row) {
+    private static Map<String, Object> getTopBlocks(String row) {
         char[]blocks = row.toCharArray();
         for (char block : blocks) {
             System.out.println( block );
         }
         return null;
+    }
+    
+    private static void compareChars () {
+        String str = "1abc";
+        int integer = 1;
+        char intChar = Integer.toString(integer).charAt(0);
+        Character convertedInt = new Character((char) integer);
+        List<Character> listOfChars = new ArrayList<Character>();
+        
+        char[] charArray = str.toCharArray();
+        for (char ch : charArray) {
+            listOfChars.add( ch);
+        }
+        
+        System.out.println( listOfChars.contains( intChar ) );
+        System.out.println( listOfChars );
+        System.out.println( intChar );
     }
 }
